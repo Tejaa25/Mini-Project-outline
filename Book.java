ddeb095177e7a3b81871e5a8ec6da12e3612b59b
Tejaa25 <112044051+Tejaa25@users.noreply.github.com>
2022-08-25 18:23:45 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,12 @@
+import java.util.*;
+imporimportt java.util.Scanner;
+class Main {
+    public static void main(String args[])
+  {
+    ArrayList <Integer> nums =  new ArrayList <Integer> ();
+    nums.add(1);
+      System.out.println(nums);
+
+    }
+}
+ 
